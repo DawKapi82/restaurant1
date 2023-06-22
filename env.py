018158577489df61101fc99f7beef235eec745e5
DawKapi82 <dawid_kapi82@hotmail.com>
2023-06-22 13:13:10 +0000

    deployment

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://gtwtyxqt:QNUA0bh3p4gU-SxtVJv92Nb5BV2FfEJ9@lucky.db.elephantsql.com/gtwtyxqt"
+os.environ["SECRET_KEY"] = "django-insecure-c$oa-vi29(zs2gy27@1ym-e!q%_w(fc&wqf#n$7o1itwsw1x38"
+os.environ["CLOUDINARY_URL"] = "cloudinary://139425881799549:dnFa2v7QDqCPsJ8hidc3yH_knNE@deqtbjncd"
